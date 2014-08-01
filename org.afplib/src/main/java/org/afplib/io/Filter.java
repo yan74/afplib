afb722c32352b630586779de2f6f247e545f750e
Yan Hackl-Feldbusch <yan@hcsystems.de>
2014-08-01 15:18:42 +0200

    let filter throw IOExceptions

--- org.afplib/src/main/java/org/afplib/io/Filter.java
+++ org.afplib/src/main/java/org/afplib/io/Filter.java
@@ -1,11 +1,13 @@
 package org.afplib.io;
 
+import java.io.IOException;
+
 import org.afplib.base.SF;
 
 public interface Filter {
 
 	enum STATE { DROP, MODIFIED, UNTOUCHED, STOP }
 	
-	STATE onStructuredField(SF sf);
+	STATE onStructuredField(SF sf) throws IOException;
 	
 }
