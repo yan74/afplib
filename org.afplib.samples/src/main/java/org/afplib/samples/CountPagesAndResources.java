77d54e046d4d89804b9aa4b15b831dcf440d24da
Yan Hackl-Feldbusch <yan@hcsystems.de>
2014-09-22 10:08:17 +0200

    reverting back to java 7 and removing stream support

--- org.afplib.samples/src/main/java/org/afplib/samples/CountPagesAndResources.java
+++ org.afplib.samples/src/main/java/org/afplib/samples/CountPagesAndResources.java
@@ -5,14 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.Spliterator;
-import java.util.Spliterators;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import org.afplib.afplib.SFName;
 import org.afplib.base.SF;
@@ -57,67 +50,30 @@ public class CountPagesAndResources {
 			try (AfpInputStream in = new AfpInputStream(
 					new BufferedInputStream(new FileInputStream(s)))) {
 
-				AtomicInteger pages = new AtomicInteger();
-				AtomicInteger resources = new AtomicInteger();
+				int pages = 0;
+				int resources = 0;
 
-				Stream<SF> sfs;
-				
-				Iterator<SF> iter = new Iterator<SF>() {
-					SF sf = null;
-					
-					@Override
-					public boolean hasNext() {
-						try {
-							sf = in.readStructuredField();
-						} catch (IOException e) {
-							return false;
-						}
-						return sf != null;
-					}
+				SF sf;
+				while((sf = in.readStructuredField()) != null) {
+					log.trace("{}", sf);
 
-					@Override
-					public SF next() {
-						return sf;
+					switch(sf.getId()) {
+					case SFName.BPG_VALUE:
+						pages++;
+						if(progress && pages % 1000 == 0)
+							System.out.print(String.format("\r%06d %04d %s", pages, resources, s));
+						break;
+					case SFName.BRS_VALUE:
+						resources++;
+						if(progress && resources % 1000 == 0)
+							System.out.print(String.format("\r%06d %04d %s", pages, resources, s));
+						break;
 					}
-				};
-				sfs = StreamSupport.stream(Spliterators.spliteratorUnknownSize(
-		                iter, Spliterator.ORDERED | Spliterator.IMMUTABLE | Spliterator.NONNULL), false);
+				}
 				
-//				sfs = Stream.generate(new Supplier<SF>() {
-//					@Override
-//					public SF get() {
-//						try {
-//							return in.readStructuredField();
-//						} catch (IOException e) {
-//							throw new RuntimeException(e);
-//						}
-//					}
-//				});
-				
-				sfs.parallel().peek(new Consumer<SF>() {
-					@Override
-					public void accept(SF sf) {
-//						log.trace("{}", sf);
-
-						switch(sf.getId()) {
-						case SFName.BPG_VALUE:
-							int p = pages.incrementAndGet();
-							if(progress && p % 1000 == 0)
-								System.out.print(String.format("\r%06d %04d %s", p, resources.get(), s));
-							break;
-						case SFName.BRS_VALUE:
-							int r = resources.incrementAndGet();
-							if(progress && r % 1000 == 0)
-								System.out.print(String.format("\r%06d %04d %s", pages.get(), r, s));
-							break;
-						}
-					}
-				}).count();
-								
 				if(progress)
 					System.out.print("\r");
-				
-				System.out.println(String.format("%06d %04d %s", pages.get(), resources.get(), s));
+				System.out.println(String.format("%06d %04d %s", pages, resources, s));
 				
 			} catch (Exception e) {
 				if(progress)
