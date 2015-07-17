efd2a1ea36f3ceae63d5e4676fed38bb66932390
Yan Hackl-Feldbusch <yan.hackl-feldbusch@ricoh.de>
2015-07-17 07:04:57 +0200

    improved dump afp

--- org.afplib.samples/src/main/java/org/afplib/samples/DumpAFP.java
+++ org.afplib.samples/src/main/java/org/afplib/samples/DumpAFP.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 import org.afplib.base.SF;
 import org.afplib.io.AfpInputStream;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EObject;
 
 public class DumpAFP {
 
@@ -15,11 +17,29 @@ public class DumpAFP {
 			
 			SF sf;
 			while((sf = in.readStructuredField()) != null) {
-				System.out.println(sf);
+				System.out.print(sf.eClass().getName()+"  ");
+				printAttributes(sf);
+				System.out.println();
+				printContents(sf, 2);
 			}
-			
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	private static void printAttributes(EObject eobj) {
+		for(EAttribute atr : eobj.eClass().getEAllAttributes()) {
+			System.out.print(atr.getName()+":"+eobj.eGet(atr)+",");
+		}
+	}
+	
+	private static void printContents(EObject eobj, int indent) {
+		for(EObject eo : eobj.eContents()) {
+			for(int i=0; i<indent; i++) System.out.print(" ");
+			System.out.print(eo.eClass().getName()+"  ");
+			printAttributes(eo);
+			System.out.println();
+			printContents(eo, indent + 2);
 		}
 	}
 	
