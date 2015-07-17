198a4a17038bb2eb342bd1f20e07c75dc9099c9b
Yan Hackl-Feldbusch <yan.hackl-feldbusch@ricoh.de>
2015-07-17 07:03:22 +0200

    testing if java mapped file has any performance advantages over java.io streams -> doesn't look like

---
+++ org.afplib/src/test/java/org/afplib/StreamVsMappedFile.java
@@ -0,0 +1,49 @@
+package org.afplib;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.afplib.io.AfpInputStream;
+import org.afplib.io.AfpMappedFile;
+import org.junit.Test;
+
+public class StreamVsMappedFile {
+
+	private final String testFile = "testdata/bim.afp"; // TODO find some larger file
+	
+	@Test
+	public void testMappedFile() throws IOException {
+		AfpMappedFile afp = new AfpMappedFile(new File(testFile));
+		
+		// run once to load all the classes involved
+		while(afp.next() != null);
+		
+		afp = new AfpMappedFile(new File(testFile));
+		
+		long counter = 0;
+		long milis = System.nanoTime();
+		while(afp.next() != null) counter++;
+		
+		System.out.println("MappedFile: " + (System.nanoTime() - milis) / counter + " nanos per sf");		
+	}
+	
+	@Test
+	public void testStream() throws IOException {
+		AfpInputStream afp = new AfpInputStream(new BufferedInputStream(new FileInputStream(testFile)));
+		
+		// run once to load all the classes involved
+		while(afp.readStructuredField() != null);
+		afp.close();
+		
+		afp = new AfpInputStream(new BufferedInputStream(new FileInputStream(testFile)));
+		
+		long counter = 0;
+		long milis = System.nanoTime();
+		while(afp.readStructuredField() != null) counter++;
+		
+		System.out.println("Stream: " + (System.nanoTime() - milis) / counter + " nanos per sf");		
+	}
+
+}
