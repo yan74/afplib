50733c520df053fa93f16e769c9b0817375f6480
Yan Hackl-Feldbusch <yan.hackl-feldbusch@ricoh.de>
2015-07-18 09:00:57 +0200

    added reading of afp

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -1,3 +1,4 @@
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -5,6 +6,7 @@ import org.afplib.afplib.AfplibFactory;
 import org.afplib.afplib.BDT;
 import org.afplib.afplib.Comment;
 import org.afplib.afplib.EDT;
+import org.afplib.io.AfpInputStream;
 import org.afplib.io.AfpOutputStream;
 
 
@@ -26,6 +28,23 @@ public class HelloWorld {
 			aout.writeStructuredField(edt);
 			
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try(AfpInputStream ain = new AfpInputStream(new FileInputStream("hello.afp"))) {
+			
+			BDT bdt = (BDT) ain.readStructuredField();
+			EDT edt = (EDT) ain.readStructuredField();
+			
+			System.out.println(bdt.getDocName());
+			System.out.println(bdt.getReserved());
+			System.out.println(bdt.getTriplets().size());
+			System.out.println(((Comment)bdt.getTriplets().get(0)).getComment());
+
+			System.out.println(edt.getDocName());
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
