cdea3a54e2ead72d1d62d289ff8650d193171760
Yan Hackl-Feldbusch <yan@hcsystems.de>
2016-09-26 20:12:57 +0200

    0.0.12-SNAPSHOT and a new test

--- org.afplib/src/test/java/org/afplib/CodepageTest.java
+++ org.afplib/src/test/java/org/afplib/CodepageTest.java
@@ -3,17 +3,24 @@ package org.afplib;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
+import org.afplib.afplib.AfplibFactory;
+import org.afplib.afplib.AttributeValue;
 import org.afplib.afplib.BPS;
 import org.afplib.afplib.BRG;
 import org.afplib.afplib.BRS;
+import org.afplib.afplib.CGCSGID;
 import org.afplib.afplib.Comment;
 import org.afplib.afplib.EPS;
 import org.afplib.afplib.ERS;
+import org.afplib.afplib.TLE;
 import org.afplib.base.SF;
 import org.afplib.io.AfpInputStream;
+import org.afplib.io.AfpOutputStream;
 import org.junit.Test;
 
 public class CodepageTest {
@@ -23,24 +30,24 @@ public class CodepageTest {
 	 * BRG with RGrpName encoded in EBCDIC (IBM 500)
 	 * 	CGCSGID (setting codepage for following triplets to 850 - ASCII)
 	 *  Comment with CHAR encoded in ASCII
-	 * ERG 
+	 * ERG
 	 * @throws IOException
 	 */
 	@Test
 	public void testAsciiComment() throws IOException {
-		
+
 		try (AfpInputStream afpin = new AfpInputStream(new FileInputStream("testdata/asciiComment.afp"))) {
 			SF sf = afpin.readStructuredField();
 			assertTrue(sf instanceof BRG);
-			
+
 			BRG brg = (BRG) sf;
 			assertEquals("RESGRP00", brg.getRGrpName());
-			
+
 			assertTrue(brg.getTriplets().get(1) instanceof Comment);
 			Comment comment = (Comment) brg.getTriplets().get(1);
 			assertEquals("My ASCII Comment", comment.getComment());
 		}
-		
+
 	}
 
 	/**
@@ -57,42 +64,42 @@ public class CodepageTest {
 	 *  BRS (resets the triplet codepage to EBCDIC)
 	 *    ResourceObjectType
 	 *    Comment (in EBCDIC)
-	 *  ...  
-	 * ERG 
+	 *  ...
+	 * ERG
 	 * @throws IOException
 	 */
 	@Test
 	public void testAsciiAndEbcdicComment() throws IOException {
-		
+
 		try (AfpInputStream afpin = new AfpInputStream(new FileInputStream("testdata/asciiAndEbcdicComment.afp"))) {
 			SF sf = afpin.readStructuredField();
 			assertTrue(sf instanceof BRG);
-			
+
 			BRG brg = (BRG) sf;
 			assertEquals("RESGRP00", brg.getRGrpName());
-			
+
 			sf = afpin.readStructuredField();
 			assertTrue(sf instanceof BRS);
 			BRS brs = (BRS) sf;
 			assertEquals("MYRES000", brs.getRSName());
-			
+
 			assertTrue(brs.getTriplets().get(2) instanceof Comment);
 			Comment comment = (Comment) brs.getTriplets().get(2);
 			assertEquals("This is ASCII", comment.getComment());
-			
+
 			sf = afpin.readStructuredField();
 			assertTrue(sf instanceof BPS);
 			BPS bps = (BPS) sf;
 			assertTrue(bps.getTriplets().get(0) instanceof Comment);
 			comment = (Comment) bps.getTriplets().get(0);
 			assertEquals("This is EBCDIC", comment.getComment());
-			
+
 			sf = afpin.readStructuredField();
 			assertTrue(sf instanceof EPS);
-			
+
 			sf = afpin.readStructuredField();
 			assertTrue(sf instanceof ERS);
-			
+
 			sf = afpin.readStructuredField();
 			assertTrue(sf instanceof BRS);
 			brs = (BRS) sf;
@@ -102,6 +109,46 @@ public class CodepageTest {
 			comment = (Comment) brs.getTriplets().get(1);
 			assertEquals("This is EBCDIC", comment.getComment());
 		}
-		
+
+	}
+
+	@Test
+	public void testDifferentCodePageOnTLE() throws IOException {
+		ByteArrayOutputStream bout;
+		try(AfpOutputStream aout = new AfpOutputStream(bout = new ByteArrayOutputStream())) {
+            final TLE tle = AfplibFactory.eINSTANCE.createTLE();
+
+            final CGCSGID cgcsgid = AfplibFactory.eINSTANCE.createCGCSGID();
+            cgcsgid.setCPGID(850);
+            cgcsgid.setGCSGID(0);
+            tle.getTriplets().add(cgcsgid);
+
+            final AttributeValue value = AfplibFactory.eINSTANCE.createAttributeValue();
+            value.setAttVal(" abc");
+
+            tle.getTriplets().add(value);
+            aout.writeStructuredField(tle);
+		}
+
+		byte[] data = bout.toByteArray();
+		// last 4 bytes of the TLE is the attribute value
+		String testString = new String(data, data.length-4, 4, Charset.forName("ASCII"));
+		assertEquals(" abc", testString);
+
+
+		try(AfpOutputStream aout = new AfpOutputStream(bout = new ByteArrayOutputStream())) {
+            final TLE tle = AfplibFactory.eINSTANCE.createTLE();
+
+            final AttributeValue value = AfplibFactory.eINSTANCE.createAttributeValue();
+            value.setAttVal(" abc");
+
+            tle.getTriplets().add(value);
+            aout.writeStructuredField(tle);
+		}
+
+		data = bout.toByteArray();
+		// last 4 bytes of the TLE is the attribute value
+		testString = new String(data, data.length-4, 4, Charset.forName("IBM500"));
+		assertEquals(" abc", testString);
 	}
 }
