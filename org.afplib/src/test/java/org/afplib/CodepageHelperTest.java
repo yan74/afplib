71bafadecbdd2938a6108d49b9a0fbd8df3ccd7b
user <user@user-Aspire-VN7-572G>
2020-07-13 16:43:11 +0200

    caomparing charset objects, not name

--- org.afplib/src/test/java/org/afplib/CodepageHelperTest.java
+++ org.afplib/src/test/java/org/afplib/CodepageHelperTest.java
@@ -3,6 +3,8 @@ package org.afplib;
 import static org.afplib.CodepageHelper.getCharset;
 import static org.junit.Assert.assertEquals;
 
+import java.nio.charset.Charset;
+
 import org.junit.Test;
 
 public class CodepageHelperTest {
@@ -11,16 +13,16 @@ public class CodepageHelperTest {
 
 	@Test
 	public void testGetCharset() throws Exception {
-		assertEquals("IBM500", getCharset(500, UNUSED).toString());
-		assertEquals("IBM850", getCharset(850, UNUSED).toString());
+		assertEquals(Charset.forName("IBM500"), getCharset(500, UNUSED));
+		assertEquals(Charset.forName("IBM850"), getCharset(850, UNUSED));
 
-		assertEquals("UTF-16", getCharset(1200, UNUSED).toString());
-		assertEquals("windows-1252", getCharset(1252, UNUSED).toString());
-		assertEquals("IBM01141", getCharset(1141, UNUSED).toString());
+		assertEquals(Charset.forName("UTF-16"), getCharset(1200, UNUSED));
+		assertEquals(Charset.forName("windows-1252"), getCharset(1252, UNUSED));
+		assertEquals(Charset.forName("IBM01141"), getCharset(1141, UNUSED));
 
-		assertEquals("IBM273", getCharset(273, UNUSED).toString());
+		assertEquals(Charset.forName("IBM273"), getCharset(273, UNUSED));
 
-		assertEquals("IBM500", getCharset(9999999, UNUSED).toString());
+		assertEquals(Charset.forName("IBM500"), getCharset(9999999, UNUSED));
 	}
 
 }
