be0e786f30bbde0872cfd3609dcd3ec5beb33c55
A1234132 <A1234132@BE1CZ021.emea1.cds.t-internal.com>
2020-09-15 08:00:37 +0200

    Replaced IOException by new AFPFormatException if raised because of format violations

--- org.afplib/src/test/java/org/afplib/AfpInputStreamTest.java
+++ org.afplib/src/test/java/org/afplib/AfpInputStreamTest.java
@@ -1,8 +1,6 @@
 package org.afplib;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -11,8 +9,10 @@ import org.afplib.afplib.BRS;
 import org.afplib.afplib.ResourceObjectType;
 import org.afplib.base.SF;
 import org.afplib.base.Triplet;
+import org.afplib.io.AFPFormatException;
 import org.afplib.io.AfpInputStream;
 import org.eclipse.emf.common.util.EList;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AfpInputStreamTest {
@@ -67,4 +67,17 @@ public class AfpInputStreamTest {
 		
 	}
 
+	@Test(expected = AFPFormatException.class) public void testPrematureEndOfFile() throws IOException {
+		String sf = "5A001CD3A8CE000000C6F1C1F1F0F1F1F100000A21FE00";
+		byte[] b = new byte[sf.length()/2];
+		for(int i=0; i<sf.length(); i+=2) {
+			Integer ii = Integer.valueOf(sf.substring(i, i+2), 16);
+			b[i/2] = (byte) ii.intValue();
+		}
+		assertTrue((byte)0xD3 == b[3]);
+		
+		try (AfpInputStream afpin = new AfpInputStream(new ByteArrayInputStream(b))) {
+			afpin.readStructuredField();
+		}
+	}
 }
